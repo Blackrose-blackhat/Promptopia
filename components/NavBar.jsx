ac516bf91aa9beb821bbd6f70d5ebd2e0b42f445
musharaf8651 <musharafz2k3@gmail.com>
2023-07-30 22:33:57 +0530

    mobile responsive navbar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -8,6 +8,7 @@ import { signIn, signOut, useSession, getProviders } from
 const NavBar = () => {
     const isLoggedIn = true;
     const [providers, setProviders] = useState(null);
+    const [toggleDropDown, settoggleDropDown] = useState(false);
 
     useEffect(() => {
         const setProviders = async () => {
@@ -64,6 +65,57 @@ const NavBar = () => {
                     </>
                 )}
             </div>
+
+            {/*Mobile Navigation */}
+
+            <div className="sm:hidden flex relative">
+                {isLoggedIn ? (
+                    <div className="flex">
+
+                        <Image
+                            src="/assets/images/logo.svg"
+                            width={37}
+                            height={37}
+                            className="rounded-full"
+                            alt="profile"
+                            onClick={() => settoggleDropDown((prev) => !prev)}
+                        />
+                        {toggleDropDown && (
+                            <div className="dropdown">
+                                <Link
+                                    href="/profile"
+                                    className="dropdown_link"
+                                    onClick={() => settoggleDropDown(false)}
+                                >
+                                    My Profile
+                                </Link>
+                                <Link
+                                    href="/create-prompt"
+                                    className="dropdown_link"
+                                    onClick={() => settoggleDropDown(false)}
+                                >
+                                    Create Prompt
+                                </Link>
+                            </div>
+                        )}
+                    </div>
+                ) : (
+                    <>
+                        {providers && Object.values(providers).map((provider) => (
+                            <button
+                                type="button"
+                                key={provider.name}
+                                onClick={() => signIn(provider.id)}
+                                className="black_btn"
+                            >
+                                Sign In
+                            </button>
+                        ))
+                        }
+                    </>
+                )}
+            </div>
+
         </nav>
     )
 }
